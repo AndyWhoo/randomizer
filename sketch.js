36d86f28509774e651e3f083b0799787653454eb
Andy Wu <33040315+AndyWhoo@users.noreply.github.com>
2019-09-21 16:02:53 -0700

    added p5 to randomizer

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  fill(20,100,255);
+  ellipse (200, 200, 200, 200);
+  fill(210,100,50);
+  rect(60,280,400,70);
+}
